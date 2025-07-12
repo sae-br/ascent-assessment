97538b94f38c4218169e04a0697912d778e1b65c
Sarah <sarah@brownmail.ca>
2025-07-11 22:31:47 -0400

    Populate dashboard with current assessment info

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,8 +1,25 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from apps.teams.models import Team
+from apps.assessments.models import Assessment
+from datetime import date
 
 @login_required
 def dashboard_home(request):
-    user_teams = Team.objects.filter(admin=request.user)
-    return render(request, "dashboard/home.html", {"teams": user_teams})
+    teams = Team.objects.filter(admin=request.user).prefetch_related("assessments", "members")
+
+    assessments = []
+    for team in teams:
+        team.assessments.filter(deadline__gte=date.today())
+        for assessment in team.assessments.all():
+            members = team.members.all()
+            assessments.append({
+                "team": team,
+                "assessment": assessment,
+                "members": members
+            })
+
+    return render(request, "dashboard/home.html", {
+        "teams": teams,
+        "assessments": assessments
+    })
