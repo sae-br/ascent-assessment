07ae8f8f04cb9a2e1c497fb097aafaff87a8641a
Sarah <sarah@brownmail.ca>
2025-07-31 14:07:29 -0400

    Add context processor, icons to sidebar, nav link styles

---
+++ apps/common/context_processors.py
@@ -0,0 +1,19 @@
+# For breadcrumbs and associating pages with specific nav links
+
+def current_section(request):
+    path = request.path
+
+    if path.startswith("/dashboard"):
+        return {"section": "dashboard"}
+    elif path.startswith("/assessments") or "confirm" in path or "launch" in path:
+        return {"section": "assessments"}
+    elif path.startswith("/teams"):
+        return {"section": "teams"}
+    elif path.startswith("/reports"):
+        return {"section": "reports"}
+    elif path.startswith("/accounts"):
+        return {"section": "account"}
+    elif path.startswith("/payments") or path.startswith("/settings"):
+        return {"section": "other"}
+    else:
+        return {"section": ""}
