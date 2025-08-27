d9c010e4002fc0ad53a24e16c656cd6cd8c5043f
Sarah <sarah@brownmail.ca>
2025-08-27 12:03:03 -0400

    Update Reports urls, add if statement around download button

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "reports"
+
 urlpatterns = [
-    path('reports/overview/', views.reports_overview, name='reports_overview'),
-    path('reports/download/<int:report_id>/', views.download_report, name='download_report'),
-    path('generate/', views.generate_report, name='generate_report'),
-    path('team_report/', views.review_team_report_redirect, name='review_team_report_redirect'),
-    path('team_report/<int:assessment_id>/', views.review_team_report, name='review_team_report'),
+    path("overview/", views.reports_overview, name="reports_overview"),
+    path("download/<int:report_id>/", views.download_report, name="download_report"),
 ]
