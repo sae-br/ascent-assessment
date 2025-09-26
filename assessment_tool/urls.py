dc1f2ab01abdce6dba6647c63d029b5277032739
Sarah <sarah@brownmail.ca>
2025-09-26 12:29:09 -0400

    Add and link legal pages, remove unused registration in root since used in Accounts instead

--- assessment_tool/urls.py
+++ assessment_tool/urls.py
@@ -19,22 +19,27 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 def robots_txt(request):
     return render(request, "robots.txt", content_type="text/plain")
 
 urlpatterns = [
     path('', include(('apps.dashboard.urls', 'dashboard'), namespace='dashboard_root')),
-    path('alao/', admin.site.urls), #Admin Level Access Only
     path('accounts/', include(('apps.accounts.urls', 'accounts'), namespace='accounts')),
     path('assessments/', include(('apps.assessments.urls', 'assessments'), namespace='assessments')),
     path('dashboard/', include(('apps.dashboard.urls', 'dashboard'), namespace='dashboard')),
-    path('markdownx/', include('markdownx.urls')),
     path('payments/', include(('apps.payments.urls', 'payments'), namespace='payments')),
     path('pdfexport/', include('apps.pdfexport.urls')),
     path('reports/', include(('apps.reports.urls', 'reports'), namespace='reports')),
-    path("robots.txt", robots_txt, name="robots_txt"),
     path('teams/', include(('apps.teams.urls', 'teams'), namespace='teams')),
+    # Legal pages
+    path('legal/privacy/', TemplateView.as_view(template_name='legal/privacy.html'), name='privacy'),
+    path('legal/terms/',   TemplateView.as_view(template_name='legal/terms.html'),   name='terms'),
+    # Other
+    path('alao/', admin.site.urls), #Admin Level Access Only
+    path('robots.txt', robots_txt, name='robots_txt'),
+    path('markdownx/', include('markdownx.urls')),
 ]
 
 if settings.DEBUG:
